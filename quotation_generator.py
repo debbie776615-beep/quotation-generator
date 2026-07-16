6f5abbce4bb69a125ba29784774c8d780f7bfffd
debbie776615-beep <debbie776615@gmail.com>
2026-07-16 14:38:41 +0800

    Update quotation_generator.py

--- quotation_generator.py
+++ quotation_generator.py
@@ -309,7 +309,7 @@ def generate_quotation(data, output_path="quotation.pdf", logo_path=None):
     notes_table = Table(
         [[Paragraph(notes_left, STYLES["note"]),
           Paragraph("客戶簽回欄<br/>(本報價單經簽回即視為正式報價單)", STYLES["sign_label"])]],
-        colWidths=[content_width * 0.68, content_width * 0.32],
+        colWidths=[content_width * 0.8, content_width * 0.32],
     )
     notes_table.setStyle(TableStyle([
         ("BOX", (0, 0), (-1, -1), 0.75, colors.black),
